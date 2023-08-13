a8a22581af0c954b1716fd67cdc04a3e4a54e9b7
Dennis Walker <denniswalker@me.com>
2023-08-13 05:45:39 -0700

    initial commit

---
+++ configs/lspconfig.lua
@@ -0,0 +1,33 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = {
+  "html",
+  "cssls",
+  "tsserver",
+  "clangd",
+  "terraformls",
+  "golangci_lint_ls",
+  "bashls",
+  "pylsp",
+  "yamlls",
+  "helm_ls",
+  "tflint"
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig.terraformls.setup {
+  cmd = { "terraform-ls", "serve" },
+  filetypes = { "terraform", "hcl", "terraform-vars" }
+}
+-- 
+-- lspconfig.pyright.setup { blabla}
