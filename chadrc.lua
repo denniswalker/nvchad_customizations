7b1efd6e3a0dba70e848355223b3e1ad5c2991ec
Dennis Walker <denniswalker@me.com>
2023-08-13 12:21:28 -0700

    Adds cmp ui overrides.

--- chadrc.lua
+++ chadrc.lua
@@ -5,13 +5,27 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "ayu_dark",
-  theme_toggle = { "ayu_dark", "one_light" },
-
+  theme = "gatekeeper",
+  theme_toggle = { "gatekeeper", "gruvbox_light" },
+  transparency = false,
+  lsp_semantic_tokens = true,
   hl_override = highlights.override,
   hl_add = highlights.add,
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "atom_colored",
+    border_color = "teal",
+    selected_item_bg = "colored",
+  },
+  telescope = { style = "bordered"},
+  lsp = {
+    signature = {
+      disabled = true,
+      silent = true,
+    }
+  }
 }
-
 M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
