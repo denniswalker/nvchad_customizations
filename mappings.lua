b2a7fcecbeb456d051d9e99572840aaebcc36d22
Dennis Walker <denniswalker@me.com>
2023-08-27 13:04:00 -0700

    Updates chatgpt mappings.

--- mappings.lua
+++ mappings.lua
@@ -20,18 +20,18 @@ M.debug = {
 M.ChatGPT = {
   v = {
     ["<leader>cc"] = { "<cmd>ChatGPT<CR>", "ChatGPT" },
-    ["<leader>ce"] = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
-    ["<leader>cg"] = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
-    ["<leader>ct"] = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
-    ["<leader>ck"] = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
-    ["<leader>cd"] = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
-    ["<leader>ca"] = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
-    ["<leader>co"] = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
-    ["<leader>cs"] = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
-    ["<leader>cf"] = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
-    ["<leader>cx"] = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
-    ["<leader>cr"] = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
-    ["<leader>cl"] = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
+    ["<leader>ce"] = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction" },
+    ["<leader>cg"] = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction" },
+    ["<leader>ct"] = { "<cmd>ChatGPTRun translate<CR>", "Translate" },
+    ["<leader>ck"] = { "<cmd>ChatGPTRun keywords<CR>", "Keywords" },
+    ["<leader>cd"] = { "<cmd>ChatGPTRun docstring<CR>", "Docstring" },
+    ["<leader>ca"] = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests" },
+    ["<leader>co"] = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code" },
+    ["<leader>cs"] = { "<cmd>ChatGPTRun summarize<CR>", "Summarize" },
+    ["<leader>cf"] = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs" },
+    ["<leader>cx"] = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code" },
+    ["<leader>cr"] = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit" },
+    ["<leader>cl"] = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis" },
   }
 }
 
