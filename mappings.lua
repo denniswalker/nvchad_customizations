712a5206016b7b270f77376349e6a6a1658f3be1
Dennis Walker <denniswalker@me.com>
2023-09-13 08:35:23 -0700

    Updates mappings to leader d and adds lspsaga.

--- mappings.lua
+++ mappings.lua
@@ -4,38 +4,45 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<C-p>"] = {"<cmd> Telescope commands <CR>", "Open command palette."},
-    ["<M-w"] = {"<cmd> WhichKey <CR>", "Open whichkey menu."},
-    ["<C-o>"] = {"<cmd> SymbolsOutline <CR>", "Open symbols outline."},
-    ["<leader>fs"] = {"<cmd> Telescope lsp_workspace_symbols <CR>", "Find Symbols."},
+    ["<C-p>"] = { "<cmd> Telescope find_files <CR>", "Find files." },
+    ["<M-w"] = { "<cmd> WhichKey <CR>", "Open whichkey menu." },
+    ["<C-o>"] = { "<cmd> Lspsaga outline <CR>", "Open symbols outline." },
+    ["<leader>fs"] = { "<cmd> Telescope lsp_workspace_symbols <CR>", "Find Symbols." },
   },
 }
 
 -- more keybinds!
-
+M.LSPSaga = {
+  n = {
+    ["<leader>dca"] = { "<cmd>Lspsaga code_action <CR>", "Code action" },
+    ["<leader>dch"] = { "<cmd>Lspsaga hover_doc <CR>", "Hover docs" },
+    ["<leader>dcf"] = { "<cmd>Lspsaga finder <CR>", "Find references" },
+    ["<leader>dcp"] = { "<cmd>Lspsaga peek_definition <CR>", "Show definition" },
+  },
+}
 M.debug = {
   n = {
-    ["<C-d>"] = {"<cmd> lua require('dapui').toggle() <CR>", "Toggle Debug"},
-    ["<A-d>"] = {"<cmd> lua require('dap').toggle_breakpoint() <CR>", "Toggle Breakpoint"}
-  }
+    ["<leader>ddd"] = { "<cmd> lua require('dapui').toggle() <CR>", "Toggle Debug" },
+    ["<leader>ddb"] = { "<cmd> lua require('dap').toggle_breakpoint() <CR>", "Toggle Breakpoint" },
+  },
 }
 
 M.ChatGPT = {
   v = {
-    ["<leader>cc"] = { "<cmd>ChatGPT<CR>", "ChatGPT" },
-    ["<leader>ce"] = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction" },
-    ["<leader>cg"] = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction" },
-    ["<leader>ct"] = { "<cmd>ChatGPTRun translate<CR>", "Translate" },
-    ["<leader>ck"] = { "<cmd>ChatGPTRun keywords<CR>", "Keywords" },
-    ["<leader>cd"] = { "<cmd>ChatGPTRun docstring<CR>", "Docstring" },
-    ["<leader>ca"] = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests" },
-    ["<leader>co"] = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code" },
-    ["<leader>cs"] = { "<cmd>ChatGPTRun summarize<CR>", "Summarize" },
-    ["<leader>cf"] = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs" },
-    ["<leader>cx"] = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code" },
-    ["<leader>cr"] = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit" },
-    ["<leader>cl"] = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis" },
-  }
+    ["<leader>dgc"] = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+    ["<leader>dge"] = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction" },
+    ["<leader>dgg"] = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction" },
+    ["<leader>dgt"] = { "<cmd>ChatGPTRun translate<CR>", "Translate" },
+    ["<leader>dgk"] = { "<cmd>ChatGPTRun keywords<CR>", "Keywords" },
+    ["<leader>dgd"] = { "<cmd>ChatGPTRun docstring<CR>", "Docstring" },
+    ["<leader>dga"] = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests" },
+    ["<leader>dgo"] = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code" },
+    ["<leader>dgs"] = { "<cmd>ChatGPTRun summarize<CR>", "Summarize" },
+    ["<leader>dgf"] = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs" },
+    ["<leader>dgx"] = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code" },
+    ["<leader>dgr"] = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit" },
+    ["<leader>dgl"] = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis" },
+  },
 }
 
 return M
