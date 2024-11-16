cf134645f4929f4bbc8512232ed790052bab6026
Dennis Walker <denniswalker@me.com>
2024-11-16 11:22:02 -0800

    Adds avante

--- plugins.lua
+++ plugins.lua
@@ -78,12 +78,68 @@ local plugins = {
   },
 
   {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      -- add any opts here
+      provider = "openai", -- Only recommend using Claude
+      auto_suggestions_provider = "openai",
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "chatgpt-4o-latest",
+        timeout = 30000, -- Timeout in milliseconds
+        temperature = 0,
+        max_tokens = 4096,
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+
+  {
     "jackMort/ChatGPT.nvim",
     event = "VeryLazy",
     lazy = false,
     config = function()
       require("chatgpt").setup {
-        api_key_cmd = "lpass show openai_nvim_key --password",
+        api_key_cmd = "echo -n $OPENAI_API_KEY",
         -- api_host_cmd = "echo -n 'api.openai.com'"
       }
     end,
